f145933a8d8e25c78be37ab8d1c358fc985e12c7
hayashi_f <hayashi_f@PC441-hayashi-f.local>
2021-12-04 09:34:31 +0900

    first init

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+					<Link href="/posts/first-post">
+						<a>this page!</a>
+					</Link>
         </h1>
 
         <p className="description">
@@ -55,7 +59,6 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
